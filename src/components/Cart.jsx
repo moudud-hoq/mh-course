f03caded09ab520e295d41ca39d03782642dddf9
moududulhaque <moududulhaque@gmail.com>
2024-03-13 15:17:23 +0600

    S1 Call all data and showing succesfully in the console

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,9 @@
-
 const Cart = () => {
-    return (
-        <div className="bg-slate-100 h-full p-4 rounded-xl">
-            <h2>MOudfjaposfjkpoasdjfopawejfpojaefj</h2>
-        </div>
-    );
+  return (
+    <div className="bg-slate-100 h-full p-4 rounded-xl">
+      <h2>MOudfjaposfjkpoasdjfopawejfpojaefj</h2>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Courses/CoursesIndex.jsx
+++ src/components/Courses/CoursesIndex.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Course from "./Course";
 
 const CoursesIndex = () => {
+  useEffect(() => {
+    fetch("/public/coursesData.json")
+      .then((res) => res.json)
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
       <Course />
